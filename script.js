d084e1727fa495fa2c897c30b4ae2b82db54148e
Chhaya Sengar <117908142+ChhayaSengar@users.noreply.github.com>
2023-04-28 15:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let audio;
+
+function playSound(filename) {
+  stopSound(); // Stop any currently playing sounds
+
+  audio = new Audio("sounds/" + filename);
+  audio.play();
+}
+
+function stopSound() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
+
